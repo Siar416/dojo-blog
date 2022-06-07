d1a67167c69e3b3e7fe3c0ecfa05c6550d7ee037
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-07 11:58:34 -0400

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
 
@@ -21,12 +23,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
